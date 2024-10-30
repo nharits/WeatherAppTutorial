7c8802a8b26bc693428f61d6fafc2d977d1a70ce
haritS <harits4dev@gmail.com>
2024-10-30 09:46:07 +0700

    Initial Commit

---
+++ WeatherAppTutorial/WeatherAppTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppTutorialApp.swift
+//  WeatherAppTutorial
+//
+//  Created by haritS on 30/10/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
